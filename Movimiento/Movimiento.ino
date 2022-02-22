f1e8be601e132a147db3fafbb3d2bfacab6b8e2b
Marcos <arcemontecino@gmail.com>
2022-02-22 14:20:20 -0300

    update

--- Movimiento/Movimiento.ino
+++ Movimiento/Movimiento.ino
@@ -1,5 +1,6 @@
 #include <ESP_FlexyStepper.h>
-bool counter =true;
+
+bool counter = true;
 const int iman = 12;
 int count_iman = 0;
 const int MOTOR_X_STEP_PIN = 16;
@@ -30,16 +31,53 @@ void setup()
 }
 void loop()
 {
-  botella(0);
+  sbotella(4);
+  /*
 
-  delay(1000);
+  delay(5000);
   botella(1);
-  delay(1000);
+  delay(5000);
   botella(2);
-  delay(1000);
+  delay(5000);
   botella(3);
-  delay(1000);
+  delay(5000);
+  */
+}
+void sbotella(int numero_botella)
+{
 
+  stepper_X.setSpeedInStepsPerSecond(150);
+  stepper_X.setAccelerationInStepsPerSecondPerSecond(300);
+  stepper_X.setDecelerationInStepsPerSecondPerSecond(800);
+  stepper_X.setTargetPositionInSteps(-178);
+  while (!stepper_X.motionComplete())
+  {
+    stepper_X.processMovement();
+    buscar_botella(numero_botella);
+    stopmotor(4);
+    Serial.println(count_iman);
+  }
+  if (stepper_X.motionComplete())
+  {
+    count_iman = 0;
+    gohome();
+  }
+}
+
+void gohome()
+{
+  stepper_X.setSpeedInStepsPerSecond(50);
+  stepper_X.setAccelerationInStepsPerSecondPerSecond(50);
+  stepper_X.setDecelerationInStepsPerSecondPerSecond(80);
+
+  if (stepper_X.moveToHomeInSteps(-1, 5, 200, LIMIT_X_SWITCH_PIN) == true)
+  {
+    Serial.println("HOME OK");
+  }
+  else
+  {
+    Serial.println("HOME ERROR");
+  }
 }
 
 void botella(float numero)
@@ -56,7 +94,7 @@ void botella(float numero)
   stepper_X.setSpeedInStepsPerSecond(150);
   stepper_X.setAccelerationInStepsPerSecondPerSecond(300);
   stepper_X.setDecelerationInStepsPerSecondPerSecond(800);
-  stepper_X.setTargetPositionInSteps(-movimiento );
+  stepper_X.setTargetPositionInSteps(-movimiento);
   while (!stepper_X.motionComplete())
   {
     stepper_X.processMovement();
@@ -69,16 +107,24 @@ void buscar_botella(int botella)
 {
   if (digitalRead(iman) == HIGH)
   {
-    //Serial.println("iman detect");
-    if (counter == true) {
+    if (counter == true)
+    {
       count_iman = count_iman + 1;
-      counter=false;
+ 
+      counter = false;
     }
-   
-
   }
   else
   {
-    counter=true;
+    counter = true;
+  }
+}
+
+void stopmotor(int target)
+{
+  Serial.println("target");
+  if (target == count_iman)
+  {
+    Serial.println("Botella encontrada");
   }
 }
