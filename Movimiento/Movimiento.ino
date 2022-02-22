831c7f93ff62b7af01cceba9d3ad3db1c9fde709
Marcos <arcemontecino@gmail.com>
2022-02-22 14:24:51 -0300

    solo iman init

--- Movimiento/Movimiento.ino
+++ Movimiento/Movimiento.ino
@@ -1,5 +1,9 @@
 #include <ESP_FlexyStepper.h>
 
+bool counter = true;
+const int iman = 12;
+int count_iman = 0;
+const int MOTOR_X_ENABLE = 18;
 const int MOTOR_X_STEP_PIN = 16;
 const int MOTOR_X_DIRECTION_PIN = 17;
 const int LIMIT_X_SWITCH_PIN = 13;
@@ -8,12 +12,18 @@ ESP_FlexyStepper stepper_X;
 void setup()
 {
   Serial.begin(115200);
+  pinMode(iman, INPUT_PULLUP);
+  pinMode(MOTOR_X_ENABLE, OUTPUT);
+  digitalWrite(MOTOR_X_ENABLE, LOW);
 
   // HOME
   stepper_X.connectToPins(MOTOR_X_STEP_PIN, MOTOR_X_DIRECTION_PIN);
   pinMode(LIMIT_X_SWITCH_PIN, INPUT_PULLUP);
+  stepper_X.setSpeedInStepsPerSecond(50);
+  stepper_X.setAccelerationInStepsPerSecondPerSecond(50);
+  stepper_X.setDecelerationInStepsPerSecondPerSecond(80);
 
-  if (stepper_X.moveToHomeInSteps(-1, 20, 200, LIMIT_X_SWITCH_PIN) == true)
+  if (stepper_X.moveToHomeInSteps(-1, 5, 200, LIMIT_X_SWITCH_PIN) == true)
   {
     Serial.println("HOME OK");
   }
@@ -24,14 +34,53 @@ void setup()
 }
 void loop()
 {
-  botella(0);
-  delay(1000);
-  botella(1);
-  delay(1000);
-  botella(2);
-  delay(1000);
-  botella(3);
-  delay(1000);
+  sbotella(4);
+  /*
+
+    delay(5000);
+    botella(1);
+    delay(5000);
+    botella(2);
+    delay(5000);
+    botella(3);
+    delay(5000);
+  */
+}
+void sbotella(int numero_botella)
+{
+
+  stepper_X.setSpeedInStepsPerSecond(150);
+  stepper_X.setAccelerationInStepsPerSecondPerSecond(300);
+  stepper_X.setDecelerationInStepsPerSecondPerSecond(800);
+  stepper_X.setTargetPositionInSteps(-178);
+  while (!stepper_X.motionComplete())
+  {
+    stepper_X.processMovement();
+    buscar_botella(numero_botella);
+    stopmotor(4);
+    Serial.println(count_iman);
+  }
+  if (stepper_X.motionComplete())
+  {
+    count_iman = 0;
+    //gohome();
+  }
+}
+
+void gohome()
+{
+  stepper_X.setSpeedInStepsPerSecond(50);
+  stepper_X.setAccelerationInStepsPerSecondPerSecond(50);
+  stepper_X.setDecelerationInStepsPerSecondPerSecond(80);
+
+  if (stepper_X.moveToHomeInSteps(-1, 5, 200, LIMIT_X_SWITCH_PIN) == true)
+  {
+    Serial.println("HOME OK");
+  }
+  else
+  {
+    Serial.println("HOME ERROR");
+  }
 }
 
 void botella(float numero)
@@ -39,28 +88,52 @@ void botella(float numero)
   // LA DISTANCIA ENTRE CADA BOTELLA SON 50 PASOS
   // EL CICLO FOR RESTA 5 Y REPITE EL NUMERO DE LA FUNCION SUPERIOR
   // ASI SE DETERMINA LA POSICION DE FORMA AUTOMATICA
-  double movimiento = 173;
+  double movimiento = 170;
   for (int i = 0; i < numero; i++)
   {
     movimiento = movimiento - 50;
-    Serial.println(movimiento);
+    // Serial.println(movimiento);
   }
-  stepper_X.setSpeedInStepsPerSecond(200);
+  stepper_X.setSpeedInStepsPerSecond(150);
   stepper_X.setAccelerationInStepsPerSecondPerSecond(300);
   stepper_X.setDecelerationInStepsPerSecondPerSecond(800);
   stepper_X.setTargetPositionInSteps(-movimiento);
   while (!stepper_X.motionComplete())
   {
     stepper_X.processMovement();
+    buscar_botella(numero);
+    Serial.println(count_iman);
   }
 }
-/*
-**POSICION DE CADA BOTELLA
-*botella1(173);
 
-** botella2(123);
+void buscar_botella(int botella)
+{
+  if (digitalRead(iman) == HIGH)
+  {
+    if (counter == true)
+    {
+      count_iman = count_iman + 1;
 
-*** botella3 (73);
+      counter = false;
+    }
+  }
+  else
+  {
+    counter = true;
+  }
+}
 
-**** botella4(23);
-*/
+void stopmotor(int target)
+{
+  Serial.println("target");
+  if (target == count_iman)
+  {
+    delay(120);
+    stepper_X.emergencyStop();
+    delay(1000);
+    digitalWrite(MOTOR_X_ENABLE, HIGH);
+    Serial.println("Botella encontrada");
+    delay(5000);
+    gohome();
+  }
+}
