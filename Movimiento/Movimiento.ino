799bc31d71de62ddc4e93b08005b1bae53d3523b
Marcos <arcemontecino@gmail.com>
2022-02-22 16:27:09 -0300

    clean code

--- Movimiento/Movimiento.ino
+++ Movimiento/Movimiento.ino
@@ -1,28 +1,31 @@
 #include <ESP_FlexyStepper.h>
 
-bool counter = true;
-const int iman = 12;
+bool count_active_iman = true;
 int count_iman = 0;
-const int MOTOR_X_ENABLE = 18;
-const int MOTOR_X_STEP_PIN = 16;
-const int MOTOR_X_DIRECTION_PIN = 17;
-const int LIMIT_X_SWITCH_PIN = 13;
-ESP_FlexyStepper stepper_X;
+
+const int iman = 12;                  // pin para detector
+const int MOTOR_X_ENABLE = 18;        // apagar y prender motor
+const int MOTOR_X_STEP_PIN = 16;      // step pin para motor
+const int MOTOR_X_DIRECTION_PIN = 17; // direccion para motor
+const int LIMIT_X_SWITCH_PIN = 13;    // final de carrera motor
+ESP_FlexyStepper stepper_X;           // obj motor
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(115200); // serial begin
+  // pinMode para iman, motor y final de carrera
   pinMode(iman, INPUT_PULLUP);
   pinMode(MOTOR_X_ENABLE, OUTPUT);
+  pinMode(LIMIT_X_SWITCH_PIN, INPUT_PULLUP);
+
+  // estado inicial de motor
   digitalWrite(MOTOR_X_ENABLE, LOW);
 
-  // HOME
+  // GO HOME
   stepper_X.connectToPins(MOTOR_X_STEP_PIN, MOTOR_X_DIRECTION_PIN);
-  pinMode(LIMIT_X_SWITCH_PIN, INPUT_PULLUP);
   stepper_X.setSpeedInStepsPerSecond(50);
   stepper_X.setAccelerationInStepsPerSecondPerSecond(50);
   stepper_X.setDecelerationInStepsPerSecondPerSecond(80);
-
   if (stepper_X.moveToHomeInSteps(-1, 5, 200, LIMIT_X_SWITCH_PIN) == true)
   {
     Serial.println("HOME OK");
@@ -41,51 +44,45 @@ void loop()
   sbotella(2);
   delay(2000);
   sbotella(1);
-
-  /*
-
-    delay(5000);
-    botella(1);
-    delay(5000);
-    botella(2);
-    delay(5000);
-    botella(3);
-    delay(5000);
-  */
 }
 void sbotella(int numero_botella)
 {
-
+  // setup vel,aceleracion y desaceleracion
   stepper_X.setSpeedInStepsPerSecond(80);
   stepper_X.setAccelerationInStepsPerSecondPerSecond(100);
   stepper_X.setDecelerationInStepsPerSecondPerSecond(400);
-  if(numero_botella>=3){
-     stepper_X.setTargetPositionInSteps(-200);
-  }else
+  // setup distancia segun target
+  if (numero_botella >= 3)
+  {
+    stepper_X.setTargetPositionInSteps(-200);
+  }
+  else
   {
     stepper_X.setTargetPositionInSteps(-100);
   }
-
+  // procesar movimiento
   while (!stepper_X.motionComplete())
   {
-    stepper_X.processMovement();
-    buscar_botella(numero_botella);
-    stopmotor(numero_botella);
-    Serial.println(count_iman);
+    stepper_X.processMovement();    // process
+    buscar_botella(numero_botella); // buscar botella con iman
+    stopmotor(numero_botella);      // detener motor segun iman
+    Serial.println(count_iman);     // imprimir contador de imanes
   }
+  // si el movimiento se completo
   if (stepper_X.motionComplete())
   {
-    count_iman = 0;
-    gohome();
+    count_iman = 0; // reiniciar contador de imanes
+    gohome();       // volver a inicio
   }
 }
-
+// ir a inicio
 void gohome()
 {
+  // setup vel, aceleracione, desaceleracion
   stepper_X.setSpeedInStepsPerSecond(30);
   stepper_X.setAccelerationInStepsPerSecondPerSecond(50);
   stepper_X.setDecelerationInStepsPerSecondPerSecond(50);
-
+  // gohome
   if (stepper_X.moveToHomeInSteps(-1, 5, 200, LIMIT_X_SWITCH_PIN) == true)
   {
     Serial.println("HOME OK");
@@ -95,59 +92,38 @@ void gohome()
     Serial.println("HOME ERROR");
   }
 }
-
-void botella(float numero)
-{
-  // LA DISTANCIA ENTRE CADA BOTELLA SON 50 PASOS
-  // EL CICLO FOR RESTA 5 Y REPITE EL NUMERO DE LA FUNCION SUPERIOR
-  // ASI SE DETERMINA LA POSICION DE FORMA AUTOMATICA
-  double movimiento = 200;
-  for (int i = 0; i < numero; i++)
-  {
-    movimiento = movimiento - 50;
-    // Serial.println(movimiento);
-  }
-  stepper_X.setSpeedInStepsPerSecond(350);
-  stepper_X.setAccelerationInStepsPerSecondPerSecond(600);
-  stepper_X.setDecelerationInStepsPerSecondPerSecond(1000);
-  stepper_X.setTargetPositionInSteps(-movimiento);
-  while (!stepper_X.motionComplete())
-  {
-    stepper_X.processMovement();
-    buscar_botella(numero);
-    Serial.println(count_iman);
-  }
-}
-
+// detectar botellas con iman
 void buscar_botella(int botella)
 {
+  // si el iman esta
   if (digitalRead(iman) == HIGH)
   {
-    if (counter == true)
+    if (count_active_iman == true) // y el contador esta activo
     {
-      count_iman = count_iman + 1;
+      count_iman = count_iman + 1; // el contador mas 1
 
-      counter = false;
+      count_active_iman = false; // desactivar contador
     }
   }
   else
   {
-    counter = true;
+    count_active_iman = true; // si no hay iman se reactiva el iman
   }
 }
 
+// detener motor
 void stopmotor(int target)
 {
-  Serial.println("target");
+  // alcanzar objetivo
   if (target == count_iman)
   {
     delay(2000);
-    stepper_X.emergencyStop();
+    stepper_X.emergencyStop(); // detener motor
     delay(2000);
-    digitalWrite(MOTOR_X_ENABLE, HIGH);
+    digitalWrite(MOTOR_X_ENABLE, HIGH); // apagar motor
     Serial.println("Botella encontrada");
     delay(3000);
-    digitalWrite(MOTOR_X_ENABLE, LOW);
-    // gohome();
+    digitalWrite(MOTOR_X_ENABLE, LOW); // activar motor para volver al inicio
+    
   }
 }
