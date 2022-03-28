af4ba70d06dded3a60ed440b2d82112ec4e496a9
Marcos <arcemontecino@gmail.com>
2022-03-28 16:36:54 -0300

    first test

--- Movimiento/Movimiento.ino
+++ Movimiento/Movimiento.ino
@@ -7,7 +7,7 @@ const int LIMIT_X_SWITCH_PIN = 13;
 
 const int MOTOR_F_STEP_PIN = 26;
 const int MOTOR_F_DIRECTION_PIN = 27;
-//const int LIMIT_FUNNEL_SWITCH_PIN = 13;
+// const int LIMIT_FUNNEL_SWITCH_PIN = 13;
 ESP_FlexyStepper stepper_X;
 ESP_FlexyStepper stepper_FUNNEL;
 
@@ -40,7 +40,7 @@ void setup()
 void loop()
 {
   botella(0);
-  
+
   delay(1000);
   botella(1);
   delay(1000);
@@ -48,7 +48,6 @@ void loop()
   delay(1000);
   botella(3);
   delay(1000);
-  
 }
 
 void botella(float numero)
@@ -61,16 +60,37 @@ void botella(float numero)
   for (int i = 0; i < numero; i++)
   {
     movimiento = movimiento - 50;
-    //Serial.println(movimiento);
+    // Serial.println(movimiento);
   }
   stepper_X.setSpeedInStepsPerSecond(200);
   stepper_X.setAccelerationInStepsPerSecondPerSecond(300);
   stepper_X.setDecelerationInStepsPerSecondPerSecond(800);
-  stepper_X.setTargetPositionInSteps(-movimiento );
+  stepper_X.setTargetPositionInSteps(-movimiento);
   while (!stepper_X.motionComplete())
   {
     stepper_X.processMovement();
-   // buscar_botella(numero);
+    // buscar_botella(numero);
+    // stepper_FUNNEL.processMovement();
+  }
+
+  stepper_FUNNEL.setSpeedInStepsPerSecond(100);
+  stepper_FUNNEL.setAccelerationInStepsPerSecondPerSecond(300);
+  stepper_FUNNEL.setDecelerationInStepsPerSecondPerSecond(800);
+  stepper_FUNNEL.moveRelativeInMillimeters(-10);
+  while (!stepper_FUNNEL.motionComplete())
+  {
+    stepper_FUNNEL.processMovement();
+    // buscar_botella(numero);
+    // stepper_FUNNEL.processMovement();
+  }
+  delay(1000);
+
+  stepper_FUNNEL.moveRelativeInMillimeters(10);
+  while (!stepper_FUNNEL.motionComplete())
+  {
+    stepper_FUNNEL.processMovement();
+    // buscar_botella(numero);
+    // stepper_FUNNEL.processMovement();
   }
 }
 
