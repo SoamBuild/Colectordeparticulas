101ce090d955b603b0c490794169a2fc10c5197b
Marcos <arcemontecino@gmail.com>
2022-05-02 13:26:14 -0400

    single test action

--- MemoryAction/MemoryAction.ino
+++ MemoryAction/MemoryAction.ino
@@ -1,9 +1,66 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * Connect the SD card to the following pins:
+ *
+ * SD Card | ESP32
+ *    D2       -
+ *    D3       SS
+ *    CMD      MOSI
+ *    VSS      GND
+ *    VDD      3.3V
+ *    CLK      SCK
+ *    VSS      GND
+ *    D0       MISO
+ *    D1       -
+ */
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+void setup(){
+  
+  /*
+
+    Serial.begin(115200);
+    if(!SD.begin()){
+        Serial.println("Sd no montada ");
+        return;
+    }
+    uint8_t cardType = SD.cardType();
+
+    if(cardType == CARD_NONE){
+        Serial.println("No identificada");
+        return;
+    }
+    createDir(SD, "/test");
+    writeFile(SD, "/hello.txt", "Hello ");
+    */
+}
+
+
+void loop(){
+ 
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void createDir(fs::FS &fs, const char * path){
+  Serial.printf("Creating Dir: %s\n", path);
+  if(fs.mkdir(path)){
+    Serial.println("Dir created");
+  } else {
+    Serial.println("mkdir failed");
+  }
+}
+void writeFile(fs::FS &fs, const char * path, const char * message){
+  Serial.printf("Writing file: %s\n", path);
 
+  File file = fs.open(path, FILE_WRITE);
+  if(!file){
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)){
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
 }
