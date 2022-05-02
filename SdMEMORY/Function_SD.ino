0f127628c852635b6a90130dabc284876bbbf989
Marcos <arcemontecino@gmail.com>
2022-05-02 17:18:16 -0400

    Code Memory

--- SdMEMORY/Function_SD.ino
+++ SdMEMORY/Function_SD.ino
@@ -1,12 +1,12 @@
-void writeFile(fs::FS &fs, const char * path, const char * message){
+void writeFile(fs::FS &fs, const char * path, const char * message) {
   Serial.printf("Writing file: %s\n", path);
 
   File file = fs.open(path, FILE_WRITE);
-  if(!file){
+  if (!file) {
     Serial.println("Failed to open file for writing");
     return;
   }
-  if(file.print(message)){
+  if (file.print(message)) {
     Serial.println("File written");
   } else {
     Serial.println("Write failed");
@@ -14,11 +14,68 @@ void writeFile(fs::FS &fs, const char * path, const char * message){
   file.close();
 }
 
-void createDir(fs::FS &fs, const char * path){
+void createDir(fs::FS &fs, const char * path) {
   Serial.printf("Creating Dir: %s\n", path);
-  if(fs.mkdir(path)){
+  if (fs.mkdir(path)) {
     Serial.println("Dir created");
   } else {
     Serial.println("mkdir failed");
   }
 }
+void appendFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if (!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+void readFile(fs::FS &fs, const char * path){
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = fs.open(path);
+  if(!file){
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.print("Read from file: ");
+  while(file.available()){
+    Serial.write(file.read());
+  }
+  file.close();
+}
+
+
+/*
+void readFile(fs::FS &fs, const char * path) {
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = fs.open(path);
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.print("Read from file: ");
+  while (file.available()) {
+    Serial.write(file.read());
+    /*
+      char hola = file.read();
+      String myString = String(hola);
+      char Buf[6];
+
+      myString.toCharArray(Buf, 6);
+      Serial.print(myString.length());
+    
+  }
+  file.close();
+}
+*/
--- SdMEMORY/SdMEMORY.ino
+++ SdMEMORY/SdMEMORY.ino
@@ -1,42 +1,49 @@
 /*
- * Connect the SD card to the following pins:
- *
- * SD Card | ESP32
- *    D2       -
- *    D3       SS
- *    CMD      MOSI
- *    VSS      GND
- *    VDD      3.3V
- *    CLK      SCK
- *    VSS      GND
- *    D0       MISO
- *    D1       -
- */
+   Connect the SD card to the following pins:
+
+   SD Card | ESP32
+      D2       -
+      D3       SS
+      CMD      MOSI
+      VSS      GND
+      VDD      3.3V
+      CLK      SCK
+      VSS      GND
+      D0       MISO
+      D1       -
+*/
 #include "FS.h"
 #include "SD.h"
 #include "SPI.h"
-void setup(){
-  
-  
+//#include <Function_SD.ino>
+int id = 0;
+void setup() {
 
-    Serial.begin(115200);
-    if(!SD.begin()){
-        Serial.println("Sd no montada ");
-        return;
-    }
-    uint8_t cardType = SD.cardType();
 
-    if(cardType == CARD_NONE){
-        Serial.println("No identificada");
-        return;
-    }
-    createDir(SD, "/test");
-    writeFile(SD, "/Data.txt", "ID,Time,Stage,B1,B2,B3,B4");
-    
+
+  Serial.begin(115200);
+  if (!SD.begin()) {
+    Serial.println("Sd no montada ");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+
+  if (cardType == CARD_NONE) {
+    Serial.println("No identificada");
+    return;
+  }
+
+  readFile(SD, "/datatemp.txt");
+  writeFile(SD, "/Data.txt", "ID,Time,Stage,B1,B2,B3,B4\n");
+
 }
 
 
-void loop(){
- 
-
+void loop() {
+  id = id + 1;
+  String tosave = String(id) + ",000000,3,0,1,0,2\n";
+  delay(1000); 
+  appendFile(SD, "/Data.txt", tosave.c_str());
+  Serial.println(String(id) + ",000000,3,0,1,0,2\n");
+  writeFile(SD, "/dataTemp.txt", tosave.c_str());
 }
