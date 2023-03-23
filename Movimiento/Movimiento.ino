0f349626f2d4359f365230d39c2e2a47bdef91ab
FastMarcus <arcemontecino@gmail.com>
2023-03-23 17:18:36 -0300

    setup first long test

--- Movimiento/Movimiento.ino
+++ Movimiento/Movimiento.ino
@@ -1,25 +1,33 @@
 #include <ESP_FlexyStepper.h>
-
-//setup and objects stepper motors
+unsigned long tarea_inicio1;
+unsigned long tarea_inicio2;
+unsigned long tarea_inicio3;
+unsigned long tarea_inicio4;
+// setup and objects stepper motors
 const int MOTOR_X_STEP_PIN = 27;
 const int MOTOR_X_DIRECTION_PIN = 14;
 const int LIMIT_X_SWITCH_PIN = 15;
-const int MOTOR_F_STEP_PIN = 33;//33
-const int MOTOR_F_DIRECTION_PIN = 32;//32
+const int MOTOR_F_STEP_PIN = 33;      // 33
+const int MOTOR_F_DIRECTION_PIN = 32; // 32
 const int LIMIT_FUNNEL_SWITCH_PIN = 13;
 const int ENABLE_MOTORS = 26;
 ESP_FlexyStepper stepper_DISK;
 ESP_FlexyStepper stepper_FUNNEL;
 int counthomeerror = 0;
-//Sensors and actuator pins 
-int pumpwater =2;
-int imanencoder =0;
-//Control global variables
-boolean statesbotellas[]={1,0,0,0};
-void setup() {
+// Sensors and actuator pins
+int pumpwater = 2;
+int imanencoder = 0;
+// Control global variables
+boolean statesbotellas[] = {1, 1, 1, 1};
+void setup()
+{
+  tarea_inicio1 = millis();
+  tarea_inicio2 = millis();
+  tarea_inicio3 = millis();
+  tarea_inicio4 = millis();
   Serial.begin(115200);
-  pinMode(imanencoder,INPUT_PULLUP);
-  pinMode(pumpwater,OUTPUT);
+  pinMode(imanencoder, INPUT_PULLUP);
+  pinMode(pumpwater, OUTPUT);
   pinMode(ENABLE_MOTORS, OUTPUT);
   digitalWrite(ENABLE_MOTORS, LOW);
   pinMode(LIMIT_X_SWITCH_PIN, INPUT_PULLUP);
@@ -29,62 +37,92 @@ void setup() {
   homex();
   homefunnel();
   digitalWrite(ENABLE_MOTORS, HIGH);
-  
-
-
 }
-void loop() {
+void loop()
+{
+  unsigned long tiempo_transcurrido1 = millis() - tarea_inicio1;
+  unsigned long tiempo_transcurrido2 = millis() - tarea_inicio2;
+  unsigned long tiempo_transcurrido3 = millis() - tarea_inicio3;
+  unsigned long tiempo_transcurrido4 = millis() - tarea_inicio4;
 
-  searchbottle(0);
-  delay(2000);
-  searchbottle(1);
-  delay(2000);
-  searchbottle(2);
-  delay(2000);
-  searchbottle(3);
-
-}
-void searchbottle(int idbotella) {
-  
-  Serial.println("Botella: "+String(idbotella)+" Estado: "+String(statesbotellas[idbotella]));
-
-  if (statesbotellas[idbotella] == true){
-  int localdistance = 2750;
-  digitalWrite(ENABLE_MOTORS, LOW);
-  for (int i = 0; i < idbotella; i++) {
-    Serial.println(localdistance);
-    localdistance = localdistance - 800;
+  if (tiempo_transcurrido1 >= 172800000)
+  { // Cambiamos el tiempo a 2
+    tarea_inicio1 = millis();
   }
-  movex(localdistance);
-  delay(1000);
-  movefunnel(20);
-  delay(1000);
-  digitalWrite(ENABLE_MOTORS, HIGH);
-  digitalWrite(pumpwater,LOW);
-  delay(2000);
-  digitalWrite(pumpwater,HIGH);
-  delay(5000);
-  digitalWrite(ENABLE_MOTORS, LOW);
-  movefunnel(-600);
-  digitalWrite(ENABLE_MOTORS, HIGH);
-  statesbotellas[idbotella] = 0;
-  }else{
-    Serial.println("Botella "+String(idbotella)+" Completa");
+  if (tiempo_transcurrido2 >= 345600000)
+  { // Cambiamos el tiempo 4
+    searchbottle(2);
+    tarea_inicio2 = millis();
+  }
+  if (tiempo_transcurrido3 >= 518400000)
+  { // Cambiamos el tiempo a 6
+    searchbottle(1);
+    tarea_inicio3 = millis();
+  }
+  if (tiempo_transcurrido4 >= 691200000)
+  { // Cambiamos el tiempo a 8
+    searchbottle(3);
+    tarea_inicio4 = millis();
+  }
+  /*
+    searchbottle(0);
+    delay(2000);
+    searchbottle(1);
+    delay(2000);
+    searchbottle(2);
+    delay(2000);
+    searchbottle(3);
+  */
+}
+void searchbottle(int idbotella)
+{
+
+  Serial.println("Botella: " + String(idbotella) + " Estado: " + String(statesbotellas[idbotella]));
+
+  if (statesbotellas[idbotella] == true)
+  {
+    int localdistance = 2750;
+    digitalWrite(ENABLE_MOTORS, LOW);
+    for (int i = 0; i < idbotella; i++)
+    {
+      Serial.println(localdistance);
+      localdistance = localdistance - 790;
+    }
+    homex();
+    movex(localdistance);
+    delay(1000);
+    movefunnel(20);
+    delay(1000);
+    digitalWrite(ENABLE_MOTORS, HIGH);
+    digitalWrite(pumpwater, HIGH);
+    delay(8000);
+    digitalWrite(pumpwater, LOW);
+    delay(5000);
+    digitalWrite(ENABLE_MOTORS, LOW);
+    movefunnel(-600);
+    digitalWrite(ENABLE_MOTORS, HIGH);
+    statesbotellas[idbotella] = 0;
+  }
+  else
+  {
+    Serial.println("Botella " + String(idbotella) + " Completa");
   }
 }
-void movefunnel(int distance) {
+void movefunnel(int distance)
+{
   stepper_FUNNEL.setSpeedInStepsPerSecond(300);
   stepper_FUNNEL.setAccelerationInStepsPerSecondPerSecond(200);
   stepper_FUNNEL.setDecelerationInStepsPerSecondPerSecond(700);
   stepper_FUNNEL.setTargetPositionInSteps(distance);
   Serial.println("Movimiento embudo en mm: " + String(distance));
-  while (! stepper_FUNNEL.motionComplete())
+  while (!stepper_FUNNEL.motionComplete())
   {
     stepper_FUNNEL.processMovement();
   }
 }
-void homefunnel() {
-  //digitalWrite(ENABLE_MOTORS, LOW);
+void homefunnel()
+{
+  // digitalWrite(ENABLE_MOTORS, LOW);
   stepper_FUNNEL.setSpeedInStepsPerSecond(100);
   stepper_FUNNEL.setAccelerationInStepsPerSecondPerSecond(250);
   stepper_FUNNEL.setDecelerationInStepsPerSecondPerSecond(250);
@@ -93,34 +131,37 @@ void homefunnel() {
     Serial.println("HOME EMBUDO OK");
     movefunnel(-600);
     counthomeerror = 0;
-    //digitalWrite(ENABLE_MOTORS, HIGH);
+    // digitalWrite(ENABLE_MOTORS, HIGH);
   }
   else
   {
     Serial.println("HOME ERROR!" + String(counthomeerror));
     counthomeerror = counthomeerror + 1;
-    if (counthomeerror == 3) {
+    if (counthomeerror == 3)
+    {
       ESP.restart();
     }
-    else {
+    else
+    {
       homefunnel();
     }
-
   }
 }
-void movex(double distance) {
-  //digitalWrite(ENABLE_MOTORS, LOW);
+void movex(double distance)
+{
+  // digitalWrite(ENABLE_MOTORS, LOW);
   stepper_DISK.setSpeedInStepsPerSecond(400);
   stepper_DISK.setAccelerationInStepsPerSecondPerSecond(200);
   stepper_DISK.setDecelerationInStepsPerSecondPerSecond(3000);
   stepper_DISK.setTargetPositionInSteps(distance);
   Serial.println("Movimiento de disco: " + String(distance));
-  while (! stepper_DISK.motionComplete())
+  while (!stepper_DISK.motionComplete())
   {
     stepper_DISK.processMovement();
   }
 }
-void homex() {
+void homex()
+{
   stepper_DISK.setSpeedInStepsPerSecond(200);
   stepper_DISK.setAccelerationInStepsPerSecondPerSecond(1200);
   stepper_DISK.setDecelerationInStepsPerSecondPerSecond(10000);
@@ -129,18 +170,20 @@ void homex() {
   {
     Serial.println("HOME BOTELLA OK");
     delay(3000);
-    //movex(400);
+    // movex(400);
     counthomeerror = 0;
-    //digitalWrite(ENABLE_MOTORS, HIGH);
+    // digitalWrite(ENABLE_MOTORS, HIGH);
   }
   else
   {
     Serial.println("HOME ERROR!" + String(counthomeerror));
     counthomeerror = counthomeerror + 1;
-    if (counthomeerror == 3) {
+    if (counthomeerror == 3)
+    {
       ESP.restart();
     }
-    else {
+    else
+    {
       homex();
     }
   }
